1b9b8490f25d1e0dee73461fad5ca95d078f35b4
veerraj <35775015+veerraj@users.noreply.github.com>
2020-10-04 10:26:54 +0530

    update

---
+++ src/export.js
@@ -0,0 +1,7 @@
+//export variable 
+export let months = ['Jan', 'Feb', 'Mar','Apr', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+//export function
+export function sayHi(name) { 
+     console.log(name)
+ }
---
+++ src/import.js
@@ -0,0 +1,9 @@
+import { sayhi , months} from './export';
+
+// import function and call with parameters and display name in export function
+sayhi('rajveer');
+
+//import months from export file and display here
+months.forEach(month => {
+     console.log(month);
+})
